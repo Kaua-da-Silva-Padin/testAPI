aa63191ec62caec88c4f3d98d3ad0cd686f573f7
Kaua-da-Silva-Padin <kauapadin5@gmail.com>
2026-04-01 20:43:37 -0300

    Refactor dependency installation and update data fetching method in workflows

--- test/main.py
+++ test/main.py
@@ -1,17 +1,19 @@
 from playwright.sync_api import sync_playwright
 import json
+import requests
 
-def web_scrape(url: str):
-    data = {}
-    with sync_playwright() as p:
-        browser = p.chromium.launch()
-        page = browser.new_page()
-        page.goto(url)
+# def web_scrape(url: str):
+#     data = {}
+#     with sync_playwright() as p:
+#         browser = p.chromium.launch()
+#         page = browser.new_page()
+#         page.goto(url)
         
-        data['title'] = page.title()
+#         data['title'] = page.title()
         
-        return data
+#         return data
 
-data = web_scrape('https://pokemondb.net/pokedex/bulbasaur')
+# data = web_scrape('https://pokemondb.net/pokedex/bulbasaur')
+data = requests.get('https://philoapi.vercel.app/search?keyword=diogenes').json()
 with open('test/data.json', 'w') as f:
     json.dump(data, f)
