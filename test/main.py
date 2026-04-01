ad71ff3e455a8451296b47ef19ee901e92c3a57f
Kaua-da-Silva-Padin <kauapadin5@gmail.com>
2026-04-01 20:27:30 -0300

    Fix file path for JSON output in main.py

--- test/main.py
+++ test/main.py
@@ -13,5 +13,5 @@ def web_scrape(url: str):
         return data
 
 data = web_scrape('https://pokemondb.net/pokedex/bulbasaur')
-with open('data.json', 'w') as f:
+with open('test/data.json', 'w') as f:
     json.dump(data, f)
